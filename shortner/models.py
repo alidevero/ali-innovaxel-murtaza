124fe5faf481a05a7d08ddd3322a858f3d9799f6
alimurtaza <alideveromurtaza110@gmail.com>
2025-05-04 18:36:13 +0500

    feat: creadel model ShortUrl in shortner app

--- shortner/models.py
+++ shortner/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+import string , random
+from django.utils import timezone
 # Create your models here.
+
+def generate_shortcode():
+
+    return ''.join(random.choices(string.ascii_letters + string.digits , k=6))
+
+
+class ShortUrl(models.Model):
+
+    url = models.URLField()
+    short_url = models.CharField(max_length=10 , unique=True , default=generate_shortcode)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    access_count = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.short_url} -> {self.url}"
